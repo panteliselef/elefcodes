a41ab94739cc9d2c107165ffaf6e315fb74ecd69
panteliselef <panteliselef@outlook.com>
2024-09-14 23:59:48 +0300

    add clerkMiddleware

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,21 +1,3 @@
 /// <reference path="../.astro/types.d.ts" />
 
-
-// /// <reference types="astro/client" />
 /// <reference types="@clerk/astro/env" />
-
-// interface ImportMetaEnv {
-//     readonly PUBLIC_ASTRO_APP_CLERK_FRONTEND_API?: string;
-//     readonly PUBLIC_ASTRO_APP_CLERK_PUBLISHABLE_KEY?: string;
-//     readonly CLERK_API_KEY?: string;
-//     readonly CLERK_API_URL?: string;
-//     readonly CLERK_API_VERSION?: string;
-//     readonly APP_ID?: string;
-//     readonly CLERK_SECRET_KEY?: string;
-//     readonly DATABASE_URL?:string;
-//     readonly FLAG_GUESTBOOK?:string;
-// }
-
-// interface ImportMeta {
-//     readonly env: ImportMetaEnv;
-// }
