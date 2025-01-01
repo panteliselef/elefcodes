b420b8b4bfeff633a94f36b8a8e1d952e5ad6e39
panteliselef <panteliselef@outlook.com>
2025-01-01 21:52:57 +0200

    add comma

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ export default defineConfig({
     react(),
     tailwind()
   ],
-  site: "https://elef.codes"
+  site: "https://elef.codes",
   adapter: vercel({
     webAnalytics: {
       enabled: true,
